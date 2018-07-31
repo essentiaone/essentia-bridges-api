e0b2868a2f117ac1f51cc2b27906213f7c1bebb1
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2018-07-31 16:40:57 +0300

    Ethereum wallet bridge API wrapper (#2)
    Implemented:
    
        — Add documentation tree for library interface.
    
        — Add Ethereum wallet usage documentation.
    
        — Create abstract wallet class that contains general bridge API realization depends on 
    wallet name to reuse in each of supported wallets.
    
        — Create Ethereum wallet with its unique endpoints extended abstract functionality.
    
        — Add utilities for requests: create get request parameters from dictionaries.
    
    References:
        • Abstract classes — https://docs.python.org/3.6/library/abc.html
        • Property chaining — https://stackoverflow.com/questions/41817578/basic-method-chaining
        • Requests library — http://docs.python-requests.org/en/master

---
+++ bridges_api/__init__.py
@@ -0,0 +1,1 @@
+from bridges_api.main import bridges
---
+++ bridges_api/constants.py
@@ -0,0 +1,7 @@
+DEFAULT_HEADERS = {
+    'content-type': 'application/json'
+}
+
+ETHEREUM_WALLET_NAME = 'ethereum'
+
+WALLETS_BRIDGE_API_URL = 'http://185.92.221.252:2310/api/v1/'
